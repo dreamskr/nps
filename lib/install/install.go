c0328e000aa1d6c129859e0a782bea02879d5e17
dreamskr <43511466+dreamskr@users.noreply.github.com>
2026-01-06 10:53:29 +0800

    Update install.go

--- lib/install/install.go
+++ lib/install/install.go
@@ -161,7 +161,7 @@ type release struct {
 
 func downloadLatest(bin string) string {
 	// get version
-	data, err := http.Get("https://api.github.com/repos/yisier/nps/releases/latest")
+	data, err := http.Get("https://api.github.com/repos/dreamskr/nps/releases/latest")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -175,7 +175,7 @@ func downloadLatest(bin string) string {
 	fmt.Println("the latest version is", version)
 	filename := runtime.GOOS + "_" + runtime.GOARCH + "_" + bin + ".tar.gz"
 	// download latest package
-	downloadUrl := fmt.Sprintf("https://github.com/yisier/nps/releases/download/%s/%s", version, filename)
+	downloadUrl := fmt.Sprintf("https://github.com/dreamskr/nps/releases/download/%s/%s", version, filename)
 	fmt.Println("download package from ", downloadUrl)
 	resp, err := http.Get(downloadUrl)
 	if err != nil {
@@ -199,7 +199,7 @@ func downloadLatest(bin string) string {
 
 func downloadLatest2(bin string, path string) string {
 	// get version
-	data, err := http.Get("https://api.github.com/repos/yisier/nps/releases/latest")
+	data, err := http.Get("https://api.github.com/repos/dreamskr/nps/releases/latest")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -213,7 +213,7 @@ func downloadLatest2(bin string, path string) string {
 	fmt.Println("the latest version is", version)
 	filename := runtime.GOOS + "_" + runtime.GOARCH + "_" + bin + ".tar.gz"
 	// download latest package
-	downloadUrl := fmt.Sprintf("https://github.com/yisier/nps/releases/download/%s/%s", version, filename)
+	downloadUrl := fmt.Sprintf("https://github.com/dreamskr/nps/releases/download/%s/%s", version, filename)
 	fmt.Println("download package from ", downloadUrl)
 	resp, err := http.Get(downloadUrl)
 	if err != nil {
